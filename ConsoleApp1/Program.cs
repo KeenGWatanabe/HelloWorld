2049d7576ff13a10b462829b0e6a8216c1297864
60936 <studio34pl@gmail.com>
2024-08-09 17:01:14 +0800

    r

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, this is Rger!");
+        }
+    }
+}
+// stop at 16:40 
